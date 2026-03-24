56fea2337a837fee6894f58f38b41cb6ab4e006d
devsmish <smyshynskyi@gmail.com>
2026-03-24 22:51:22 +0100

    update main.py
    removed nested menus from the main file and moved
    them to a separate package

--- main.py
+++ main.py
@@ -1,57 +1,35 @@
+from app.func_menu import *
+from app.sql_connection import get_connection
+
+
 def main():
     print("Movie Search App started")
+    connection = get_connection()
+    cursor = connection.cursor()
 
     print("""
-=========================SEARCH MENU=========================""")
+=========================SEARCH APP=========================""")
 
     while True:
         print("""
-========================ГЛАВНОЕ МЕНЮ=========================
+========================ГЛАВНОЕ МЕНЮ========================
 Выберите опцию, которой вы хотели бы воспользоваться (1, 2 или 0):
 1. Если хотите найти фильм.
 2. Если хотите посмотреть статистику поиска.
 0. Если хотите завершить программу.""")
         initial_choice = input("Enter your option: ")
         if initial_choice == "1":
-            while True:
-                print("""
-=========================МЕНЮ ПОИСКА=========================
-Выберите критерий для поиска фильма (1, 2 или 0):
-1. Поиск по ключевому слову.
-2. Поиск по жанру или году выпуска.
-0. Вернуться в предыдущее меню.""")
-                search_choice = input("Enter your criterion: ")
-                if search_choice == "1":
-                    pass
-                elif search_choice == "2":
-                    pass
-                elif search_choice == "0":
-                    break
-                else:
-                    print("Invalid criterion. Please try again.")
+            search_menu(cursor)
         elif initial_choice == "2":
-            while True:
-                print("""
-======================МЕНЮ СТАТИСТИКИ========================
-Выберите вариант статистики для просмотра (1, 2 или 0):
-1. ТОП5 поисковых запросов.
-2. 5 последних поисковых запросов.
-0. Вернуться в предыдущее меню.""")
-                statistic_choice = input("Enter your statistic: ")
-                if statistic_choice == "1":
-                    pass
-                elif statistic_choice == "2":
-                    pass
-                elif statistic_choice == "0":
-                    break
-                else:
-                    print("Invalid criterion. Please try again.")
+            stats_menu()
         elif initial_choice == "0":
             print("App closed. Goodbye!")
             break
         else:
             print("Invalid choice. Please try again.")
 
+    cursor.close()
+    connection.close()
+
 if __name__ == "__main__":
     main()
-    
