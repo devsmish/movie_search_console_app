3a5a15a512d75c5e75e57cdb480769d7ad344f06
devsmish <smyshynskyi@gmail.com>
2026-03-25 22:11:25 +0100

    add mongo_queries.py description log examples and create mongodb queries (top5 and last5)

---
+++ app/mongo_queries.py
@@ -0,0 +1,60 @@
+
+log_example_key = {
+    "timestamp": "2026-03-25T15:34:00",
+    "search_type": "keyword",
+    "params": {
+        "keyword": "matrix",
+    },
+    "results_count": 3,
+    "duration_ms": 340,
+    "success": true,
+    "query_key": "JIN"
+}
+
+log_example_gy = {
+    "timestamp": "2026-03-26T16:54:18",
+    "search_type": "genres-years",
+    "params": {
+        "genres": "Action",
+        "years": {
+            "start_year": 1990,
+            "end_year": 2025,
+        }
+ },
+    "results_count": 250,
+    "duration_ms": 250,
+    "success": false,
+    "query_key": "Games_2005_2010"
+}
+
+top5_queries = [
+    {
+        "$group": {
+            "_id": "$query_key",
+            "count": {"$sum": 1}
+        }
+    },
+    {
+        "$sort": {"count": -1}
+    },
+    {
+        "$limit": 5
+    }
+]
+
+last5_queries = [
+  {
+    "$project": {
+      "_id": 0,
+      "params": 0
+    }
+  },
+  {
+    "$sort": {
+      "timestamp": -1
+    }
+  },
+  {
+    "$limit": 5
+  }
+]
